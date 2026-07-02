2dd5e62dc7231ca2ef11c522ddd1960b809f2a89
Yuichi-dev565 <icho.papa.tmp@gmail.com>
2026-07-02 13:31:03 +0900

    Update test.js

--- test.js
+++ test.js
@@ -27,8 +27,19 @@
             }
         }
         // --- ここまでエラーチェックの条件 ---
-
         return event; // eventオブジェクトを返すことでエラーが反映される
     });
+
+    // レコード詳細画面が表示された時のイベント
+    kintone.events.on('app.record.detail.show', function(event) {
+        
+        // 画面に表示したいメッセージ
+        var infoMessage = '【お知らせ】\n本レコードの確認事項です。\n期日までに対応を行ってください。';
+
+        // アラートでメッセージを表示
+        alert(infoMessage);
+
+        return event;
+
 })();
 
